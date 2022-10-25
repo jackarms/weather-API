be2d221f4ed380b00185a9ffcafdc935b6b3542d
jackarms <90922918+jackarms@users.noreply.github.com>
2022-10-24 21:28:24 -0500

    Current forecast and 3-Day forecase implemented

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ async function fetchWeather(city) {
 
 async function fetchFiveDay(lat, lon) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key2}`
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
   );
   const fiveDay = await response.json();
-  console.log(fiveDay);
+  return fiveDay;
 }
 
 const getDate = function () {
@@ -33,8 +33,8 @@ const getDate = function () {
 const skyColor = function () {
   const d = new Date();
   let hour = d.getHours();
-
-  if (hour > 19 || hour < 4) {
+  console.log(hour);
+  if ((hour) => 19 || hour < 4) {
     container.classList.add("containerNight");
   } else if (hour > 4 && hour < 10) {
     container.classList.add("containerMorning");
@@ -45,14 +45,23 @@ const skyColor = function () {
 
 skyColor();
 
+const unixToDay = function (timeStamp) {
+  const milli = timeStamp * 1000;
+  const d = new Date(milli);
+  const month = d.getMonth();
+  const day = d.getDay();
+  const year = d.getFullYear();
+  return d.toLocaleString("en-US", { weekday: "long" });
+};
+
 const buildContainer = function (city, temp, humidity, wind) {
-  const divEl = document.createElement("div");
-  const h1El = document.createElement("h1");
-  const h2El = document.createElement("h2");
-  const p1El = document.createElement("p");
-  const p2El = document.createElement("p");
-  const p3El = document.createElement("p");
-  const p4El = document.createElement("p");
+  const divEl = document.getElementById("result-div");
+  const h1El = document.getElementById("result-h1");
+  const h2El = document.getElementById("result-h2");
+  const p1El = document.getElementById("result-p1");
+  const p2El = document.getElementById("result-p2");
+  const p3El = document.getElementById("result-p3");
+  const p4El = document.getElementById("result-p4");
   h1El.textContent = `${city} Weather`;
   h2El.textContent = `Today's date is ${getDate()}`;
   p1El.textContent = `The temperature is ${temp}℉`;
@@ -66,10 +75,9 @@ const buildContainer = function (city, temp, humidity, wind) {
 btnEl.addEventListener("click", () => {
   const city = document.getElementById("input").value;
   if (city == "Hogwarts") {
-    alert("Ello Harry... in Honor Of That Guy Who Played Hagrid");
+    container.classList.add("containerHagrid");
   }
   fetchWeather(city).then((results) => {
-    console.log(results);
     const lat = results.coord.lat;
     const lon = results.coord.lon;
     fetchFiveDay(lat, lon);
@@ -79,5 +87,20 @@ btnEl.addEventListener("click", () => {
       results.main.humidity,
       results.wind.speed
     );
+    fetchFiveDay(lat, lon).then((results) => {
+      console.log(results);
+      for (i = 1; i <= 3; i++) {
+        const divEl = document.getElementById(`card${[i]}`);
+        const h3El = document.getElementById(`h3-${[i]}`);
+        const p1El = document.getElementById(`p${[i]}-1`);
+        const p2El = document.getElementById(`p${[i]}-2`);
+        const p3El = document.getElementById(`p${[i]}-3`);
+        h3El.textContent = `On ${unixToDay(results.daily[i].dt)}`;
+        p1El.textContent = `The temperature will be ${results.daily[i].temp.day}℉`;
+        p2El.textContent = `The humidity will be ${results.daily[i].humidity}%`;
+        p3El.textContent = `With a ${results.daily[i].rain}% chance of rain`;
+        divEl.classList.add(`card${[i]}`);
+      }
+    });
   });
 });
